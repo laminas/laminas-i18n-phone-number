ca7e403f5b02017644100e67c850e5be0e70dbd3
George Steel <george@net-glue.co.uk>
2022-06-10 15:52:40 +0100

    Improve Module and ConfigProvider
    - Add explanatory notes about the purpose of the classes
    - improve type inference for consumers
    
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- src/Module.php
+++ src/Module.php
@@ -4,8 +4,26 @@ declare(strict_types=1);
 
 namespace Laminas\I18n\PhoneNumber;
 
+use Laminas\ServiceManager\ConfigInterface;
+
+/**
+ * A module class for use with Laminas MVC applications
+ *
+ * @link ConfigInterface
+ *
+ * @psalm-import-type ServiceManagerConfigurationType from ConfigInterface
+ * @psalm-import-type PackageConfig from ConfigProvider
+ */
 final class Module
 {
+    /**
+     * @return array{
+     *     laminas-i18n-phone-number: PackageConfig,
+     *     filters: ServiceManagerConfigurationType,
+     *     validators: ServiceManagerConfigurationType,
+     *     view_helpers: ServiceManagerConfigurationType,
+     * }
+     */
     public function getConfig(): array
     {
         return (new ConfigProvider())();
