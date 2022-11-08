b1c94db9602e1bfa68b85bfa3dbc120ebd7e5001
George Steel <george@net-glue.co.uk>
2022-11-08 12:19:57 +0000

    Require config aggregator, adding an abstract 'ProjectIntegrationTestCase' to ease testing integration with other laminas components
    Signed-off-by: George Steel <george@net-glue.co.uk>

---
+++ test/ProjectIntegrationTestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\I18n\PhoneNumber\Test;
+
+use Laminas;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @psalm-import-type ServiceManagerConfigurationType from Laminas\ServiceManager\ConfigInterface
+ * @phpcs:disable WebimpressCodingStandard.NamingConventions.AbstractClass.Prefix
+ */
+abstract class ProjectIntegrationTestCase extends TestCase
+{
+    private const CONFIG_PROVIDERS = [
+        Laminas\Filter\ConfigProvider::class,
+        Laminas\Form\ConfigProvider::class,
+        Laminas\I18n\ConfigProvider::class,
+        Laminas\I18n\PhoneNumber\ConfigProvider::class,
+        Laminas\InputFilter\ConfigProvider::class,
+        Laminas\Validator\ConfigProvider::class,
+    ];
+
+    protected static function getContainer(array $userConfig = []): ContainerInterface
+    {
+        $providers   = self::CONFIG_PROVIDERS;
+        $providers[] = new Laminas\ConfigAggregator\ArrayProvider($userConfig);
+
+        $aggregator   = new Laminas\ConfigAggregator\ConfigAggregator($providers);
+        $config       = $aggregator->getMergedConfig();
+        $dependencies = $config['dependencies'] ?? [];
+        self::assertIsArray($dependencies);
+        /** @psalm-var ServiceManagerConfigurationType $dependencies */
+        $dependencies['services']['config'] = $config;
+
+        return new Laminas\ServiceManager\ServiceManager($dependencies);
+    }
+}
